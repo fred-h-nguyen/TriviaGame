57dab4877736739679b776e04b124aa0c2e6733c
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-08 16:53:38 -0500

    pseudocoded the game also made variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,89 @@
-$().ready(function(){
+$().ready(function () {
+
+    var intervalID;
+    var timeLeft = 120;
+    var q1 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q2 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q3 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q4 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q5 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q6 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q7 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var q8 = {
+        question: '',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        ans: ''
+    };
+    var correct = 0;
+    var wrong = 0;
+    var unanswered = 0;
+
+
+    //make a start button on'click event which starts timer
+
+    //make a timer that counts down from 120seconds using set interval
+    //apply the interval to DOM timerbox
+    //show a question and possible answers from array of objects.
+
+    // on click of answer reveal if correct wait 3 seconds then next question in array.
+    //or if timer hits 0
+
+    // if answer correct++ if answer wrong++ if unanswered ++
+
+    // if correct + wrong + unanswered = array length stop and reveal scorebox,
+    //restart button
 
 
 
